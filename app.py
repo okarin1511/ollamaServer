3fff9768fc4bc1de9e8fcde7c2ae9ee9178356f7
Aseem Khandekar <aseem.khandekar@gmail.com>
2024-11-14 18:52:03 +0530

    fix: port

--- app.py
+++ app.py
@@ -57,4 +57,4 @@ async def generateText(request: Request) -> Response:
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=5001)
+    uvicorn.run(app, host="0.0.0.0", port=11434)
