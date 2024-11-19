2795cad42ede363233265f65de641a9415f901cf
Aseem Khandekar <aseem.khandekar@gmail.com>
2024-11-19 17:59:04 +0530

    fix: improve

--- app.py
+++ app.py
@@ -10,25 +10,36 @@ import hashlib
 import uvicorn
 from huggingface_hub import login
 from langchain_huggingface import HuggingFacePipeline
-from transformers import AutoConfig
 import torch
-from transformers import pipeline
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 
 app = FastAPI()
 
 login("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")
 
 model_id = "meta-llama/Llama-3.2-3B-Instruct"
+
+model = AutoModelForCausalLM.from_pretrained(
+    model_id,
+    torch_dtype=torch.bfloat16,
+    device_map="auto",
+)
+
+tokenizer = AutoTokenizer.from_pretrained(model_id)
 pipe = pipeline(
     "text-generation",
-    model=model_id,
+    model=model,
     torch_dtype=torch.bfloat16,
     trust_remote_code=True,
-    max_new_tokens=50,
+    max_new_tokens=100,
     temperature=0.2,
+    tokenizer=tokenizer,
     top_p=0.95,  # Use nucleus sampling to focus on top 95% of options
     top_k=50,
+    pad_token_id=tokenizer.eos_token_id,
+    eos_token_id=tokenizer.eos_token_id,
     device_map="auto",
+    return_full_text=False,
 )
 
 llm = HuggingFacePipeline(pipeline=pipe)
@@ -61,14 +72,15 @@ async def generateText(request: Request) -> JSONResponse:
     request_dict = await request.json()
     prompt = request_dict.pop("prompt")
 
-    llmResponse = llm.invoke([prompt])
+    formatted_prompt = f"[INST] {prompt} [/INST]"
 
-    print("OUTPUTT", llmResponse)
+    # Generate response
+    llmResponse = llm.invoke(formatted_prompt)
+    print("Generated text:", llmResponse)
 
     end_time = time.time()
     latency = end_time - start_time
     print(f"Latency: {latency} seconds")
 
-    print("Generated text:", llmResponse)
     ret = {"response": llmResponse, "latency": latency}
     return JSONResponse(ret)
