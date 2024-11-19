6363133b8037154a0f82e8ef61e6e02c4bc7cbe0
Aseem Khandekar <aseem.khandekar@gmail.com>
2024-11-19 11:12:11 +0530

    feat: model

--- app.py
+++ app.py
@@ -32,11 +32,11 @@ def init_gptcache(cache_obj: Cache, llm: str):
 
 langchain.llm_cache = GPTCache(init_gptcache)
 llm = VLLM(
-    model="meta-llama/Llama-3.2-1B-Instruct",
+    model="meta-llama/Llama-3.2-3B-Instruct",
     trust_remote_code=True,  # mandatory for hf models
-    max_new_tokens=100,
+    max_new_tokens=75,
     temperature=0.6,
-    dtype="float32",
+    dtype="float16",
     gpu_memory_utilization="0.95",
 )
 
